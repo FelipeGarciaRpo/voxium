3cedaba6ab85f41aadcd949a71ddba3cf4fd81c7
felipe <felipe.garciar@upb.edu.co>
2026-04-22 05:22:58 -0500

    06: tts generation audio player

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,11 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { createTRPCRouter } from '../init';
+// import { billingRouter } from './billing';
+import { generationsRouter } from './generations';
 import { voicesRouter } from './voices';
- 
 export const appRouter = createTRPCRouter({
   voices: voicesRouter,
-})
-
+  generations: generationsRouter,
+  // billing: billingRouter,
+});
 // export type definition of API
 export type AppRouter = typeof appRouter;
