56beab7fa7c19d5264b1fa6bc583fff50294a064
felipe <felipe.garciar@upb.edu.co>
2026-04-22 01:43:52 -0500

    05: voice selection

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -8,9 +8,9 @@ import { createTRPCContext } from '@trpc/tanstack-react-query';
 import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
- 
+import superjson from 'superjson';
+
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
- 
 let browserQueryClient: QueryClient;
 function getQueryClient() {
   if (typeof window === 'undefined') {
@@ -24,7 +24,7 @@ function getQueryClient() {
   if (!browserQueryClient) browserQueryClient = makeQueryClient();
   return browserQueryClient;
 }
- 
+
 function getUrl() {
   const base = (() => {
     if (typeof window !== 'undefined') return '';
@@ -33,7 +33,7 @@ function getUrl() {
   })();
   return `${base}/api/trpc`;
 }
- 
+
 export function TRPCReactProvider(
   props: Readonly<{
     children: React.ReactNode;
@@ -49,7 +49,7 @@ export function TRPCReactProvider(
     createTRPCClient<AppRouter>({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
